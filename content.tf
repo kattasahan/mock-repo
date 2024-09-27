72b5200fa78f377f445ed8c18e4c913c52bc5bbb
Katta, Sahan <Sahan.Katta@carelon.com>
2024-09-27 19:40:16 +0530

    add code in files types: .tf remove code in files types: .tf

--- content.tf
+++ content.tf
@@ -2,4 +2,4 @@ resource "random_string" "sqowa" { length = 10 }
 resource "random_string" "wswof" { length = 10 }
 resource "random_string" "fgpjy" { length = 10 }
 resource "random_string" "vtush" { length = 10 }
-resource "random_string" "vihyg" { length = 10 }
+resource "random_string" "iicli" { length = 10 }
